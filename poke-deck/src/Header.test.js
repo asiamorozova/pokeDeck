45bdd1c2176282f363d587e7fca15f2a941f701f
asiamorozova <57855398+asiamorozova@users.noreply.github.com>
2020-02-13 10:44:41 -0800

    testing done

---
+++ poke-deck/src/Header.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Header from './Header.js';
+
+test ('This render Header', () => {
+    const header = renderer
+    .create (<Header/>)
+    .toJSON ();
+    expect (header).toMatchSnapshot();
+})
