3dbd24810ed63437a94f7aaa6f88de0e33fd68d1
asiamorozova <57855398+asiamorozova@users.noreply.github.com>
2020-02-13 10:27:30 -0800

    saving chages

--- poke-deck/src/pokelist.js
+++ poke-deck/src/pokelist.js
@@ -8,6 +8,7 @@ export default class Container extends Component {
                 <h2>{this.props.pokemon.pokemon}</h2></div>
             <p className="type1-container">Type 1:{this.props.pokemon.type_1}</p>
             <p className="type2-container">Type 2: {this.props.pokemon.type_2}</p>
+    <p className="speed-contaniner">Speed: {this.props.pokemon.speed}</p>
             <div className="image-container">
                 <img
                     alt={this.props.pokemon.url_image}
